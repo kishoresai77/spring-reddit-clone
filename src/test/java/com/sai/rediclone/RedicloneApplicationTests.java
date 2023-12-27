f34eb50c44a51c0e30f4e2b342c237f511609bc5
saikishorereddy <saikishorereddyvallapu@gmail.com>
2023-12-27 21:35:04 +0800

    reditclone initial setup

---
+++ src/test/java/com/sai/rediclone/RedicloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.rediclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedicloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
